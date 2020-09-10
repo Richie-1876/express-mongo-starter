534c47093c227bf3c7acfffe4e96c79c11d5fd36
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-09-10 18:36:05 +0100

    Changed DB variables to facilitate migration from mlab to atlas

--- server.js
+++ server.js
@@ -1,74 +1,79 @@
 //___________________
 //Dependencies
 //___________________
-const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
+const express = require("express");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 const app = express();
 const db = mongoose.connection;
-const session = require('express-session')
+const session = require("express-session");
 //___________________
 //Port
 //___________________
 // Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 3000;
-require('dotenv').config()
+require("dotenv").config();
 
 //___________________
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/recipes';
+const MONGODB_URI = process.env.DB_URI || "mongodb://localhost:27017/recipes";
 
 // Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 // open the connection to mongo
-db.on('open' , ()=>{});
+db.on("open", () => {});
 
 //___________________
 //Middleware
 //___________________
 
 //use public folder for static assets
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
+app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
 
 //use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //___________________
 // Controllers
 // __________________
-const recipeController = require('./controllers/recipe_controller.js')
-app.use('/recipes', recipeController)
-const sessionsController = require('./controllers/sessions_controller.js')
-app.use('/sessions', sessionsController)
-const userController = require('./controllers/users_controller.js')
-app.use('/users', userController)
+const recipeController = require("./controllers/recipe_controller.js");
+app.use("/recipes", recipeController);
+const sessionsController = require("./controllers/sessions_controller.js");
+app.use("/sessions", sessionsController);
+const userController = require("./controllers/users_controller.js");
+app.use("/users", userController);
 
 //___________________
 // Routes
 //___________________
 //localhost:3000
-app.get('/' , (req, res) => {
-  res.redirect('/recipes');
+app.get("/", (req, res) => {
+  res.redirect("/recipes");
 });
 
 //___________________
 //Listener
 //___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+app.listen(PORT, () => console.log("Listening on port:", PORT));
